4b0ef1634d2070862c396c049e84b856be116ddb
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2020-03-15 03:43:16 +0900

    update test

--- email/sample_email_builder.py
+++ email/sample_email_builder.py
@@ -16,8 +16,6 @@ to_addr = 'hello-hdr@example.jp'
 from_addr = 'hello-hdr@example.com'
 
 def make_simple_part_email():
-    envelope_from = 'hello@example.com'
-
     msg = MIMEText("テスト\n\nあいうえお")
     msg['Subject'] = 'タイトル'
     msg['From'] = from_addr
@@ -26,34 +24,48 @@ def make_simple_part_email():
 
     return msg
 
-def make_multi_part_email():
+def make_english_email():
+    msg = MIMEText("English Email")
+    msg['Subject'] = 'My Subject'
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Date'] = formatdate()
 
-    body = "テスト\n\nあいうえお"
+    return msg
+
+def make_iso_2022_jp_charset_email():
+    msg = MIMEText("ISO-20220-JP\nで作られた本文。", _charset="iso-2022-jp")
+    msg['Subject'] = Header('ISO-2022-JPのタイトル', "iso-2022-jp").encode()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Date'] = formatdate()
+
+    return msg
+
+def make_multi_part_email():
+    body = "添付ファイルが2個ついているメール"
 
     mime = MIMEMultipart()
     mesg = MIMEText(_text=body, _subtype='plain', _charset='utf-8')
     mime.attach(mesg)
 
-    mime['Subject'] = 'My Subject'
+    mime['Subject'] = 'マルチパートのメール'
     mime['From'] = from_addr
     mime['To'] = to_addr
     mime['Date'] = formatdate(localtime=True)
 
-    part = MIMEBase('plain', 'text')
-    part.set_payload('ファイルの中身')
-    encoders.encode_base64(part)
-    attach_filename = Header('添付ファイル1.txt', 'utf-8').encode()
-    part.set_param('name', attach_filename)
-    part.add_header("Content-Disposition", "attachment", filename=attach_filename)
-    mime.attach(part)
-
-
-    part = MIMEBase('plain', 'text')
-    part.set_payload('file contents')
-    encoders.encode_base64(part)
-    attach_filename = Header('attachment-file2.txt', 'utf-8').encode()
-    part.set_param('name', attach_filename)
-    part.add_header("Content-Disposition", "attachment", filename=attach_filename)
-    mime.attach(part)
+    mime.attach(_attachment('日本語ファイル.txt', 'utf-8', 'ファイルの中身'))
+    mime.attach(_attachment('english-file.txt', 'utf-8', 'file contents'))
 
     return mime
+
+def _attachment(file_name, file_name_charset, payload):
+    part = MIMEBase('plain', 'text')
+    part.set_payload(payload)
+    encoders.encode_base64(part)
+    
+    filename = Header(file_name, file_name_charset).encode()
+    part.set_param('name', filename)
+    part.add_header("Content-Disposition", "attachment", filename=filename)
+
+    return part
--- email/test.py
+++ email/test.py
@@ -11,7 +11,19 @@ class TestMessageInspector(unittest.TestCase):
 
     def test_multi_part_email(self):
         mail = builder.make_multi_part_email()
-        smtp_server.MessageInspector.inspect_from_bytes('test@example.com', ['test@example.com'], mail.as_bytes())
+        self._inspect(mail)
+
+    def test_no_charset_email(self):
+        mail = builder.make_english_email()
+        self._inspect(mail)
+
+    def test_iso_2022_jp(self):
+        mail = builder.make_iso_2022_jp_charset_email()
+        self._inspect(mail)
+
+    def _inspect(self, mail):
+        data = mail.as_bytes()
+        smtp_server.MessageInspector.inspect_from_bytes('test@example.com', ['test@example.com'], data)
 
 if __name__ == "__main__":
     unittest.main()

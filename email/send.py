5fc2395e0c3c6a1eb0be6e2f2b9689be103fb16c
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2020-03-15 20:43:52 +0900

    change certificate

--- email/send.py
+++ email/send.py
@@ -2,14 +2,22 @@
 
 import smtplib
 import sample_email_builder as builder
+import ssl
 
 mailsv = 'localhost'
+# mailsv = 'dummy-smtp.example.jp'
+
 port = 10587
 
 msg = builder.make_simple_part_email()
 smtp = smtplib.SMTP(mailsv, port)
 smtp.ehlo()
-smtp.starttls()
+
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+context.load_verify_locations(cafile='keys/server.crt')
+# context.load_verify_locations(cafile='keys/error-keys/server.crt')
+smtp.starttls(context=context)
+
 smtp.ehlo()
 smtp.login('myid', 'mypass')
 smtp.sendmail(builder.envelope_from, builder.to_addr, msg.as_string())

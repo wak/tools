59508a63c89849ec36955c8776935fefa72d7845
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2021-10-01 23:31:10 +0900

    update

--- python/zipcode/main.py
+++ python/zipcode/main.py
@@ -4,10 +4,20 @@ import sys
 import logging
 
 import zipcode
+from pprint import pprint
+
+# a = zipcode.JapanPostZipcodeAddress('12345678', 'test1', 'a', '')
+# b = zipcode.JapanPostZipcodeAddress('12345678', 'test1', 'b', '')
+# pprint(a & b)
+# exit()
 
 logging.basicConfig(level=logging.DEBUG)
 jpzipmap = zipcode.JapanPostZipcodeMap('ken_all.zip')
 
 zipcode = sys.argv[1]
 print(f'specified zip code: {zipcode}')
-print(jpzipmap.get_address(zipcode))
+print('-- common --')
+pprint(jpzipmap.find_common_address(zipcode))
+
+print('\n-- all --')
+pprint(jpzipmap.find_all_addresses(zipcode))
--- python/zipcode/zipcode.py
+++ python/zipcode/zipcode.py
@@ -6,73 +6,87 @@ import csv
 import io
 import collections
 
-class JapanPostZipcodeAddress(object):
-    def __init__(self, row):
-        self.zipcode = row[2]        # 3. 郵便番号(7桁)
-        self.todofuken = row[6]      # 7. 都道府県名
-        self.shikuchoson = row[7]    # 8. 市区町村名
-        self.choiki = row[8]         # 9. 町域名
-        if self.choiki == '以下に掲載がない場合':
-            self.choiki = ''
-
-    def values(self):
-        return [self.todofuken, self.shikuchoson, self.choiki]
-
-    def address(self):
-        return ''.join(self.values())
-
-    def __eq__(self, other):
-        if isinstance(other, JapanPostZipcodeAddress):
-            return self.address() == other.address()
-        return False
-
 class JapanPostZipcodeMap(object):
     Address = collections.namedtuple('Address', 'address all')
 
     def __init__(self, ken_all_path):
         self._zipcode_map = dict()
+        self._load_ken_all_zip(ken_all_path)
 
+    def _load_ken_all_zip(self, ken_all_path):
         nr_rows = 0
         zip = zipfile.ZipFile(ken_all_path)
         with zip.open(zip.infolist()[0], 'r') as in_stream:
             with io.TextIOWrapper(in_stream, 'shift_jis_2004') as in_stream2:
                 reader = csv.reader(in_stream2)
                 for row in reader:
-                    self._process_zip_csv_row(row)
                     nr_rows += 1
+                    self._register_address(
+                        row[2],             # 3. 郵便番号(7桁)
+                        JapanPostZipcodeAddress.from_raw_line(row)
+                    )
         logging.info(f'Japan Post ZIP: %d rows found.', nr_rows)
 
-    def _process_zip_csv_row(self, row):
-        self._register_address(
-            row[2],             # 3. 郵便番号(7桁)
-            JapanPostZipcodeAddress(row)
-        )
-
     def _register_address(self, zipcode, address):
-        if zipcode in self._zipcode_map:
-            if address in self._zipcode_map[zipcode]:
-                # logging.info('duplicated zip address: %s', repr(all_data))
-                pass
-            else:
-                self._zipcode_map[zipcode].append(address)
-        else:
-            self._zipcode_map[zipcode] = [address]
+        self._zipcode_map.setdefault(zipcode, [])
+        if address in self._zipcode_map[zipcode]:
+            return
 
-    def get_addresses(self, zipcode):
+        self._zipcode_map[zipcode].append(address)
+
+    def find_all_addresses(self, zipcode):
         if not zipcode in self._zipcode_map:
             return []
         return self._zipcode_map[zipcode]
 
-    def get_address(self, zipcode):
-        return self._get_common_address(self.get_addresses(zipcode))
+    def find_common_address(self, zipcode):
+        if not zipcode in self._zipcode_map:
+            return None
+        result = self._zipcode_map[zipcode][0]
+        for address in self._zipcode_map[zipcode]:
+            result &= address
+        return result
 
-    def _get_common_address(self, addresses):
-        if not addresses:
-            return []
+class JapanPostZipcodeAddress(object):
+    def __init__(self, zipcode, todofuken, shikuchoson, choiki):
+        self.zipcode = zipcode
+        self.todofuken = todofuken
+        self.shikuchoson = shikuchoson
+        self.choiki = choiki
 
-        for i in range(3):
-            compareing = addresses[0].values()[i]
-            for a in addresses:
-                if compareing != a.values()[i]:
-                    return ''.join(a.values()[0:i])
-        return a.address()
+    @classmethod
+    def from_raw_line(cls, row):
+        zipcode = row[2]        # 3. 郵便番号(7桁)
+        todofuken = row[6]      # 7. 都道府県名
+        shikuchoson = row[7]    # 8. 市区町村名
+        choiki = row[8]         # 9. 町域名
+        if choiki == '以下に掲載がない場合':
+            choiki = ''
+        return cls(zipcode, todofuken, shikuchoson, choiki)
+
+    def address(self):
+        return ''.join([self.todofuken, self.shikuchoson, self.choiki])
+
+    def __eq__(self, other):
+        if isinstance(other, JapanPostZipcodeAddress):
+            return self.address() == other.address()
+        return False
+
+    def __and__(self, other):
+        if self.zipcode != other.zipcode:
+            raise Exception('zipcode should equal')
+
+        todofuken = ''
+        shikuchoson = ''
+        choiki = ''
+        if self.todofuken == other.todofuken:
+            todofuken = self.todofuken
+            if self.shikuchoson == other.shikuchoson:
+                shikuchoson = self.shikuchoson
+                if self.choiki == other.choiki:
+                    choiki = self.choiki
+        return JapanPostZipcodeAddress(self.zipcode, todofuken, shikuchoson, choiki)
+
+
+    def __repr__(self):
+        return f'<JapanPostZipcodeAddress {self.zipcode}, {self.todofuken}, {self.shikuchoson}, {self.choiki}>'
